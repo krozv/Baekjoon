e7f5c15acb0ec7994cc9141662b6a76aba53981d
krozv <krozv.code@gmail.com>
2024-01-16 23:02:26 +0900

    [Bronze I] Title: 단어 공부, Time: 2324 ms, Memory: 111592 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,18 @@
-input_data = list(input())
-upper_input = []
-count_input = []
-# input의 알파벳을 전부 large로 변경
-for i in range (len(input_data)):
-    upper_input.append(input_data[i].upper())
-# print(upper_input)
+alphabet = list(input())
 
-# 사용된 알파벳 리스트 형성
-upper_input.sort()
-used_alphabet = list(set(upper_input))
+# input의 알파벳을 전부 large로 변경
+alphabet = [i.upper() for i in alphabet]
+alphabet.sort()
+used_alphabet = list(set(alphabet))
 
 # 알파벳마다 사용횟수 count_input 리스트에 작성
-for j in range (len(used_alphabet)):
-    count_input.append(upper_input.count(used_alphabet[j]))
-Max = max(count_input)
+count_input = []
+for i in used_alphabet:
+    count_input.append(alphabet.count(i))
+Max_alphabet = max(count_input)
 
 # output
-if count_input.count(Max) > 1:
+if count_input.count(Max_alphabet) > 1:
     print('?')
 else:
-    print(used_alphabet[count_input.index(Max)])
+    print(used_alphabet[count_input.index(Max_alphabet)])
