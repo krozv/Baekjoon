a1d79d2470ed0f31ea8b4a959fd7ef3bec240e70
krozv <krozv.code@gmail.com>
2024-01-30 20:16:22 +0900

    [Bronze II] Title: 수 정렬하기, Time: 192 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,11 @@
+# 2750. 수 정렬하기
+
+N = int(input())
+num_list = []
+for _ in range(N):
+    num_list.append(int(input()))
+for i in range(N-1):
+    for j in range(i+1, N):
+        if num_list[i] > num_list[j]:
+            num_list[i], num_list[j] = num_list[j], num_list[i]
+print(*num_list, sep='\n')
