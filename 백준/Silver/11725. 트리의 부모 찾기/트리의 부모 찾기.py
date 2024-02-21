ed95513db1610c3f56b28c893fefb574ec6196c6
krozv <krozv.code@gmail.com>
2024-02-21 19:17:02 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 328 ms, Memory: 65756 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,36 @@
+# 11725. tree fine parent
+"""
+tree parent fine.
+my pycharm is broken
+no korean...
+why? why?
+condition
+tree without root
+root = 1
+who is parent of node?
+"""
+def find_parent(p):
+    parent = p
+    if adj[p]:
+        for c in adj[p]:
+            par[c] = p  # 자식을 인덱스로 부모를 저장함
+            adj[c].remove(p)
+            p = c
+            find_parent(p)
+            p = parent
+
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+N = int(input())
+par = [0] * (N+1)
+adj = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    n1, n2 = map(int, input().split())
+    adj[n1].append(n2)
+    adj[n2].append(n1)
+
+find_parent(1)
+for i in par[2:]:
+    print(i)
